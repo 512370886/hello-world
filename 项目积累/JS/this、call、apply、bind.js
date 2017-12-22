9625b61fe08637a509a4d78baa76222e47a84b5b
liangcaiyuan0806 <LIANGCAIYUAN669@pingan.com.cn>
2017-12-22 08:36:06 +0800

    更改提交

--- 项目积累/JS/this、call、apply、bind.js
+++ 项目积累/JS/this、call、apply、bind.js
@@ -1,6 +1,9 @@
-这又是一个面试经典问题~/(ㄒoㄒ)/~~也是 ES5中众多坑中的一个，在 ES6 中可能会极大避免 this 产生的错误，但是为了一些老代码的维护，最好还是了解一下 this 的指向和 call、apply、bind 三者的区别。
+这又是一个面试经典问题~/(ㄒoㄒ)/~~也是 ES5中众多坑中的一个，在 ES6 中可能会极大避免 this 产生的错误，
+但是为了一些老代码的维护，最好还是了解一下 this 的指向和 call、apply、bind 三者的区别。
 this 的指向
-在 ES5 中，其实 this 的指向，始终坚持一个原理：this 永远指向最后调用它的那个对象，来，跟着我朗读三遍：this 永远指向最后调用它的那个对象，this 永远指向最后调用它的那个对象，this 永远指向最后调用它的那个对象。记住这句话，this 你已经了解一半了。
+在 ES5 中，其实 this 的指向，始终坚持一个原理：this 永远指向最后调用它的那个对象，来，
+跟着我朗读三遍：this 永远指向最后调用它的那个对象，
+this 永远指向最后调用它的那个对象，this 永远指向最后调用它的那个对象。记住这句话，this 你已经了解一半了。
 下面我们来看一个最简单的例子：
 例 1：
 var name = "windowsName";
@@ -11,7 +14,10 @@ function a() {
 }
 a();
 console.log("outer:" + this)         // outer: Window
-这个相信大家都知道为什么 log 的是 windowsName，因为根据刚刚的那句话“this 永远指向最后调用它的那个对象”，我们看最后调用 a 的地方 a();，前面没有调用的对象那么就是全局对象 window，这就相当于是 window.a()；注意，这里我们没有使用严格模式，如果使用严格模式的话，全局对象就是 undefined，那么就会报错 Uncaught TypeError: Cannot read property ‘name’ of undefined。
+这个相信大家都知道为什么 log 的是 windowsName，因为根据刚刚的那句话“this 永远指向最后调用它的那个对象”，
+我们看最后调用 a 的地方 a();，前面没有调用的对象那么就是全局对象 window，这就相当于是 window.a()；注意，
+这里我们没有使用严格模式，如果使用严格模式的话，全局对象就是 undefined，
+那么就会报错 Uncaught TypeError: Cannot read property ‘name’ of undefined。
 再看下这个例子：
 例 2：
 var name = "windowsName";
@@ -44,8 +50,12 @@ var a = {
     }
 }
 window.a.fn();
-这里为什么会打印 undefined 呢？这是因为正如刚刚所描述的那样，调用 fn 的是 a 对象，也就是说 fn 的内部的 this 是对象 a，而对象 a 中并没有对 name 进行定义，所以 log 的 this.name 的值是 undefined。
-这个例子还是说明了：this 永远指向最后调用它的那个对象，因为最后调用 fn 的对象是 a，所以就算 a 中没有 name 这个属性，也不会继续向上一个对象寻找 this.name，而是直接输出 undefined。
+这里为什么会打印 undefined 呢？这是因为正如刚刚所描述的那样，调用 fn 的是 a 对象，
+也就是说 fn 的内部的 this 是对象 a，而对象 a 中并没有对 name 进行定义，
+所以 log 的 this.name 的值是 undefined。
+
+这个例子还是说明了：this 永远指向最后调用它的那个对象，因为最后调用 fn 的对象是 a，
+所以就算 a 中没有 name 这个属性，也不会继续向上一个对象寻找 this.name，而是直接输出 undefined。
 再来看一个比较坑的例子：
 例 5：
 var name = "windowsName";
@@ -58,8 +68,11 @@ var a = {
 }
 var f = a.fn;
 f();
-这里你可能会有疑问，为什么不是 Cherry，这是因为虽然将 a 对象的 fn 方法赋值给变量 f 了，但是没有调用，再接着跟我念这一句话：“this 永远指向最后调用它的那个对象”，由于刚刚的 f 并没有调用，所以 fn() 最后仍然是被 window 调用的。所以 this 指向的也就是 window。
-由以上五个例子我们可以看出，this 的指向并不是在创建的时候就可以确定的，在 es5 中，永远是this 永远指向最后调用它的那个对象。
+这里你可能会有疑问，为什么不是 Cherry，这是因为虽然将 a 对象的 fn 方法赋值给变量 f 了，
+但是没有调用，再接着跟我念这一句话：“this 永远指向最后调用它的那个对象”，由于刚刚的 f 并没有调用，
+所以 fn() 最后仍然是被 window 调用的。所以 this 指向的也就是 window。
+由以上五个例子我们可以看出，this 的指向并不是在创建的时候就可以确定的，
+在 es5 中，永远是this 永远指向最后调用它的那个对象。
 再来看一个例子：
 例 6：
 var name = "windowsName";
@@ -92,10 +105,13 @@ var a = {
     }
 };
 a.func2()     // this.func1 is not a function
-在不使用箭头函数的情况下，是会报错的，因为最后调用 setTimeout 的对象是 window，但是在 window 中并没有 func1 函数。
+在不使用箭头函数的情况下，是会报错的，因为最后调用 setTimeout 的对象是 window，
+但是在 window 中并没有 func1 函数。
 我们在改变 this 指向这一节将把这个例子作为 demo 进行改造。
 箭头函数
-众所周知，ES6 的箭头函数是可以避免 ES5 中使用 this 的坑的。箭头函数的 this 始终指向函数定义时的 this，而非执行时。，箭头函数需要记着这句话：“箭头函数中没有 this 绑定，必须通过查找作用域链来决定其值，如果箭头函数被非箭头函数包含，则 this 绑定的是最近一层非箭头函数的 this，否则，this 为 undefined”。
+众所周知，ES6 的箭头函数是可以避免 ES5 中使用 this 的坑的。箭头函数的 this 始终指向函数定义时的 this，
+而非执行时。，箭头函数需要记着这句话：“箭头函数中没有 this 绑定，必须通过查找作用域链来决定其值，
+如果箭头函数被非箭头函数包含，则 this 绑定的是最近一层非箭头函数的 this，否则，this 为 undefined”。
 例 8 ：
 var name = "windowsName";
 var a = {
@@ -111,7 +127,8 @@ var a = {
 };
 a.func2()     // Cherry
 在函数内部使用 _this = this
-如果不使用 ES6，那么这种方式应该是最简单的不会出错的方式了，我们是先将调用这个函数的对象保存在变量 _this 中，然后在函数中都使用这个 _this，这样 _this 就不会改变了。
+如果不使用 ES6，那么这种方式应该是最简单的不会出错的方式了，我们是先将调用这个函数的对象保存在变量 _this 中，
+然后在函数中都使用这个 _this，这样 _this 就不会改变了。
 例 9：
 var name = "windowsName";
 var a = {
@@ -128,7 +145,9 @@ var a = {
     }
 };
 a.func2()       // Cherry
-这个例子中，在 func2 中，首先设置 var _this = this;，这里的 this 是调用 func2 的对象 a，为了防止在 func2 中的 setTimeout 被 window 调用而导致的在 setTimeout 中的 this 为 window。我们将 this(指向变量 a) 赋值给一个变量 _this，这样，在 func2 中我们使用 _this 就是指向对象 a 了。
+这个例子中，在 func2 中，首先设置 var _this = this;，这里的 this 是调用 func2 的对象 a，
+为了防止在 func2 中的 setTimeout 被 window 调用而导致的在 setTimeout 中的 this 为 window。
+我们将 this(指向变量 a) 赋值给一个变量 _this，这样，在 func2 中我们使用 _this 就是指向对象 a 了。
 使用 apply、call、bind
 使用 apply、call、bind 函数也是可以改变 this 的指向的，原理稍后再讲，我们先来看一下是怎么实现的：
 使用 apply
--- 项目积累/JS/原生JS实现AJAX、JSONP及DOM加载完成事件，并提供对应方法.js
+++ 项目积累/JS/原生JS实现AJAX、JSONP及DOM加载完成事件，并提供对应方法.js
@@ -1,5 +1,6 @@
 JS原生AJAX
-ajax：一种请求数据的方式，不需要刷新整个页面； ajax的技术核心是 XMLHttpRequest 对象； ajax 请求过程：创建 XMLHttpRequest 对象、连接服务器、发送请求、接收响应数据；
+ajax：一种请求数据的方式，不需要刷新整个页面； ajax的技术核心是 XMLHttpRequest 对象； 
+ajax 请求过程：创建 XMLHttpRequest 对象、连接服务器、发送请求、接收响应数据；
 下面简单封装一个函数，之后稍作解释
 ajax({
         url: "./testXhr.php",              //请求地址
